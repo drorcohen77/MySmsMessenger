11510b865e6c823e4b1295c4cbe6e9192ceeffc0
Dror Cohen <drorcohen@Drors-MBP-2.lan>
2022-12-01 13:17:34 +0200

    adding css for responsive

--- server/main.py
+++ server/main.py
@@ -1,21 +1,39 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify, Response, request
 from flask_cors import CORS
-from flask_pymongo import PyMongo
+import json
+import pymongo
 import send_messages
 import certifi
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb+srv://dror:Almog20haifa!@cluster0.vk6vnph.mongodb.net/SMSDB?retryWrites=true&w=majority'
 mongo = PyMongo()
+mongo.init_app(app, tlsCAFile=certifi.where())
 
 CORS(app)
-mongo.init_app(app, tlsCAFile=certifi.where())
+
+### when using comase app locally
+# try:
+#     mongo = pymongo.MongoClient(
+#       host="localhost",
+#       port="27017",
+#       serverSelectionTimeoutMS=1000
+#     )
+#     db = mongo.SMSDB
+#     mongo.server_info()   # trigger exception if connection to db not succied
+# except:
+#     print("EROR - Cannot connect to DB")
 
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    messages_collection = mongo.db.mySMSmessanger
-    messages_list = list(messages_collection.find({}, {"_id": 0}))
+    try:
+        messages_collection = mongo.db.mySMSmessanger
+        messages_list = list(messages_collection.find({}, {"_id": 0}))
+    except Exception as ex:
+        print(ex)
+
     return jsonify(messages_list)
 
 
@@ -24,11 +42,24 @@ def add_message():
     data = request.json
     destination = destination_fixer(data['phoneNumber'])
     message = data['message']
-    send_messages.send_message(destination, message)
+    # send_messages.send_message(destination, message)
 
-    messages_list = mongo.db.mySMSmessanger
-    messages_list.insert_one(data)
-    return get_messages()
+    try:
+        messages_list = mongo.db.mySMSmessanger
+        db_response = messages_list.insert_one(data)
+        print(db_response.inserted_id)
+
+        # db_response = db.mySMSmessanger.insert_one(data)  ### when using comase app locally
+        # print(db_response.inserted_id)
+    except Exception as ex:
+        print(ex)
+
+    # return get_messages()
+    return Response(
+      response=json.dumps({"message": "user created", "id": f'{db_response.inserted_id}'}),
+      status=200,
+      mimetype="application/json"
+    )
 
 
 def destination_fixer(phone):
@@ -37,5 +68,6 @@ def destination_fixer(phone):
     return destination
 
 
-app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
 
